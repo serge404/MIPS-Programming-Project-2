59e508aa8f600c1d8a6955c955221c9ceccecaaa
Serge Selenou <32373603+serge404@users.noreply.github.com>
2018-11-16 16:27:06 -0500

    project2.s
    Created data header

---
+++ project2.s
@@ -0,0 +1,9 @@
+.data
+	char_array: .space 5 # space needed for 4 characters and 1 null
+	invalid_hex: .asciiz "Invalid base-30 number" #for characters not in the set
+	hex_too_long: .asciiz "Input is too long." #for strings more than 5 characters
+	hex_too_short: .asciiz "Input is empty." #for strings with 0 characters
+
+.text 
+	main:
+	
