a5079a986cee4d51efd63e666fa282bdc488f5cb
Serge Selenou <32373603+serge404@users.noreply.github.com>
2018-11-16 20:17:50 -0500

    Initializing

--- project2.s
+++ project2.s
@@ -1,5 +1,5 @@
 .data
-	char_array: .space 5 # space needed for 4 characters and 1 null
+	user_string: .space 5 # space needed for 4 characters and 1 null
 	invalid_hex: .asciiz "Invalid base-30 number" #for characters not in the set
 	hex_too_long: .asciiz "Input is too long." #for strings more than 5 characters
 	hex_too_short: .asciiz "Input is empty." #for strings with 0 characters
@@ -7,7 +7,13 @@
 .text 
 	main:
         li $v0, 8 # read in a string
-        la $a0, char_array
-        li $a1, 9
+        la $a0, user_string
+        li $a1, 2500
         syscall
-	
+
+	la $t0, user_string
+	li $s0, 0	# store value
+	li $s7, 0	# checks for spaces
+	li $t4, 1	# power that is multiplied
+	li $s1, 27				
+	li $t1, 0   	# counts the number of characters in string
